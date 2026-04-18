1c5df7994f4864ac3f3edd75099db0c26b3f9fd4
wadpas <wadpas@gmail.com>
2026-04-18 14:54:12 +0300

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
-import { Arimo } from 'next/font/google'
+import { Arimo, Geist } from 'next/font/google'
 import '@/assets/styles/globals.css'
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const lato = Arimo({
   weight: ['400'],
@@ -18,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' className={cn("font-sans", geist.variable)}>
       <body className={`${lato.className}  antialiased`}>{children}</body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Image from 'next/image'
+import logo from '../public/logo.svg'
+
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <Image
+        src={logo}
+        alt='logo'
+        width={100}
+        height={100}></Image>
     </div>
   )
 }
